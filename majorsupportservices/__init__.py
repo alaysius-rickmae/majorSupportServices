75da16c1dc449704a355820a877d0b3785f743e2
unknown <addressrepo2025@gmail.com>
2026-01-30 12:40:19 +1100

    add static in link for images

--- majorsupportservices/__init__.py
+++ majorsupportservices/__init__.py
@@ -4,7 +4,9 @@ from flask import Flask
 def create_app():
     app = Flask(__name__)
     from .index import index
+    from .about import about
 
     app.register_blueprint(index, url_prefix='/')
+    app.register_blueprint(about, url_prefix='/about')
   
     return app
--- majorsupportservices/about.py
+++ majorsupportservices/about.py
@@ -1,18 +1,10 @@
 from datetime import date, timedelta
 from flask import Blueprint, json, render_template, request, session, jsonify, redirect, url_for
 
-index = Blueprint('index', __name__)
+index = Blueprint('about', __name__)
 
 
 @index.route('/')
 def dashboard():
     
-    return render_template('index.html')
-
-
-
-    
-@index.errorhandler(404)
-def invalid_route(e):
-
-    return render_template('404.html')
+    return render_template('about.html')
