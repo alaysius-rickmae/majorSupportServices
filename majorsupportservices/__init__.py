adc919363e84481b57068707394e13e9ab13edb7
unknown <addressrepo2025@gmail.com>
2026-02-03 13:10:50 +1100

    update colors

--- majorsupportservices/__init__.py
+++ majorsupportservices/__init__.py
@@ -5,8 +5,24 @@ def create_app():
     app = Flask(__name__)
     from .index import index
     from .contact import contact
+    from .about import about
+    from .service import service
+    from .blog import blog
+    from .service_details import service_details
+    from .team import team
+    from .team_details import team_details
+    from .portfolio import portfolio
+    from .portfolio_details import portfolio_details    
 
     app.register_blueprint(index, url_prefix='/')
     app.register_blueprint(contact, url_prefix='/')
+    app.register_blueprint(about, url_prefix='/')
+    app.register_blueprint(service, url_prefix='/')
+    app.register_blueprint(blog, url_prefix='/')
+    app.register_blueprint(service_details, url_prefix='/')
+    app.register_blueprint(team, url_prefix='/')
+    app.register_blueprint(team_details, url_prefix='/')
+    app.register_blueprint(portfolio, url_prefix='/')
+    app.register_blueprint(portfolio_details, url_prefix='/')
   
     return app
